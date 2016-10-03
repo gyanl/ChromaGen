873306ca140c6450d3a3a78b41f28b27c26f3407
collnwalkr <collinwalkerdesigns@gmail.com>
2016-10-03 12:20:15 -0700

    fix copy to clipboard error for keyboard shorcut as well as added keyboard navigation between colors

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 /**
-* @Author: gyanl, ananayarora
-* @Date:   2016-09-29T09:56:56+05:30
+* @Author: gyanl, ananayarora, colllnwalkr
+* @Date:   2016-10-03T09:56:56+05:30
 * @Last modified by:   ananayarora
-* @Last modified time: 2016-09-29T10:00:14+05:30
+* @Last modified time: 2016-10-03T10:00:14+05:30
 */
 
 var newColour = [];
 var format = 'hex';
+var currentColor = 0;
+var numButtons = 6;
 
 function changeColorFormat() {
     format = document.querySelector('input[type="radio"]:checked').value;
@@ -26,6 +28,7 @@ function genColor() {
 
 function changeColour(num) {
     if (num != -1) {
+        currentColor = num;
         new_color = newColour[num];
         //outline();  either use the fn or just write the code here.
     } else {
@@ -37,11 +40,17 @@ function changeColour(num) {
             document.getElementById('c0').focus();
         }
     }
-    document.body.style.backgroundColor = new_color;
-    document.getElementById('clipboard').style.background = new_color;
-    document.getElementById('generate').style.background = new_color;
-    document.getElementById('copyall').style.background = new_color;
-    document.getElementById('clipboard').setAttribute("data-clipboard-text", formatColor(new_color));
+    updateUI(new_color);
+}
+
+function updateUI(color){
+    if(color){
+        document.body.style.backgroundColor = color;
+        document.getElementById('clipboard').style.background = color;
+        document.getElementById('generate').style.background = color;
+        document.getElementById('copyall').style.background = color;
+        document.getElementById('clipboard').setAttribute("data-clipboard-text", formatColor(color));
+    }
 }
 
 function formatColor(color) {
@@ -50,8 +59,9 @@ function formatColor(color) {
 }
 
 function toast(yo) {
-    yo.setAttribute("data-clipboard-text", formatColor(newColour[0]));
+    yo.setAttribute("data-clipboard-text", formatColor(newColour[currentColor]));
     document.getElementById("toast").innerHTML = yo.getAttribute("data-clipboard-text") + " copied to clipboard.";
+    copyTextToClipboard(formatColor(newColour[currentColor]));
 }
 
 function copyAll(yo) {
@@ -70,6 +80,27 @@ function start() {
     changeColour(-1);
 }
 
+function shiftColorLeft(num){
+    console.log(num);
+    num -= 1;
+    if(num < 0){
+        num = numButtons - 1;
+    }
+    document.getElementById('c' + num).focus()
+    updateUI(newColour[num]);
+    currentColor = num;
+}
+
+function shiftColorRight(num){
+    num += 1;
+    if(num >= numButtons){
+        num = 0;
+    }
+    document.getElementById('c' + num).focus()
+    updateUI(newColour[num]);
+    currentColor = num;
+}
+
 document.onkeydown = function(e) {
     switch (e.keyCode) {
         case 82:
@@ -85,5 +116,44 @@ document.onkeydown = function(e) {
         var color = document.getElementById('copyall');
         copyAll(color)
         break;
+
+        case 37:
+        shiftColorLeft(currentColor)
+        break;
+
+        case 39:
+        shiftColorRight(currentColor)
+        break;
     }
 };
+
+// From: Dean Taylor at http://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+function copyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+
+    textArea.style.position = 'fixed';
+    textArea.style.top = 0;
+    textArea.style.left = 0;
+    textArea.style.width = '2em';
+    textArea.style.height = '2em';
+    textArea.style.padding = 0;
+    textArea.style.border = 'none';
+    textArea.style.outline = 'none';
+    textArea.style.boxShadow = 'none';
+
+    textArea.style.background = 'transparent';
+
+
+    textArea.value = text;
+
+    document.body.appendChild(textArea);
+
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+    } catch (err) {
+    }
+
+    document.body.removeChild(textArea);
+}
