de5cb26c31e81a4692003b34474d9521d812955b
gyanl <gyanlakhwani@gmail.com>
2017-09-29 16:47:13 +0530

    Revert "Merge pull request #17 from NielsKornerup/gh-pages"
    This reverts commit 0fbc7b5d393b19ade0665f93137bf60c377efd2b, reversing
    changes made to 7c1543d425dafb888e0df72a00faa5293d1bd69a.

--- js/script.js
+++ js/script.js
@@ -7,9 +7,6 @@
 
 var newColour = [];
 var format = 'hex';
-//The index of the currently selected color
-var currentColour = 0;
-var numColours = 0;
 var currentColor = 0;
 var numButtons = 6;
 var listOfIndexes = [0,1,2,3,4,5];
@@ -54,20 +51,6 @@ function genColor() {
     return "#" + a;
 }
 
-function changeColour(num) {
-    if (num != -1) {
-        currentColour = num;
-        new_color = newColour[currentColour];
-        document.getElementById('c'+currentColour).focus();
-        //outline();  either use the fn or just write the code here.
-    } else {
-        currentColour = 0;
-        new_color = genColor();
-        newColour.unshift(new_color);
-        if (numColours<6){
-            numColours++;
-        }
-      
 // Returns either a single color, or a gradient definition
 function colorString(color, fullStyle) {
     if (!color) {
@@ -136,7 +119,6 @@ function formatColor(color) {
 }
 
 function toast(yo) {
-    yo.setAttribute("data-clipboard-text", formatColor(newColour[currentColour]));
     synchronize();
     var style = colorString(newColour[currentColor], true);
     yo.setAttribute("data-clipboard-text", style);
@@ -177,30 +159,14 @@ document.onkeydown = function(e) {
 
         case 67:
         var color = document.getElementById('clipboard');
-        color.click();
+        toast(color)
         break;
 
         case 65:
         var color = document.getElementById('copyall');
-        color.click();
+        copyAll(color)
         break;
 
-        case 37:
-        if(currentColour>0){
-            changeColour(currentColour-1);
-        }
-        else{
-            changeColour(numColours-1);
-        }
-        break;
-
-        case 39:
-        if(currentColour<numColours-1){
-            changeColour(currentColour+1);
-        }
-        else{
-            changeColour(0);
-        }
         case 37:
         shiftColor(-1)
         break;
